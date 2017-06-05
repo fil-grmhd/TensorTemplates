db11902bb1e518a1fd5c1052aa22bf088fda846e
Elias Roland Most <eliasroland.most@stud.uni-frankfurt.de>
2017-06-05 11:32:41 +0200

    Matrix vector multiplication works. Matrix-matrix not yet. Will debug this over the coming days.

--- src/utils/tensor_expression_test.cc
+++ src/utils/tensor_expression_test.cc
@@ -61,8 +61,57 @@ int main(){
 
   vector3_t<double> f = contract<1,0>(M,a) + c - 3.15*a +b/2.;
 
+  std::cout << "f = M*a +c - 3.15*a + b/2" <<std::endl;
   std::cout << "f :" << f[0] <<" , "<< f[1] << " , " << f[2] <<std::endl;
+  std::cout<<"According to Matlab:\n" << "f : 16.35 , 30.7 , 45.05" <<std::endl;
 
+
+  tensor3_t<double, upper_t, lower_t> A,B;
+
+  A[0] = 1; A[1] = 14.5; A[2] = 0.1278;
+  A[3] = -12;  A[4] = 7834; A[5] = 0.002; 
+  A[6] = -8; A[7] = 1.e-4; A[8] = 919;
+
+  B[0] = 0; B[1] = 1.5; B[2] = 34;
+  B[3] = -3;  B[4] = 7834; B[5] = 5404; 
+  B[6] = -3459; B[7] = 2300; B[8] = 93;
+
+
+  std::cout << "A = " << std::endl; 
+  for(int i=0; i<3; ++i){
+    for(int j=0; j<3; ++j)
+      std::cout << " "<< A[i+3*j] << " ";
+    std::cout<<std::endl;
+  }
+
+  std::cout << "B = " << std::endl; 
+  for(int i=0; i<3; ++i){
+    for(int j=0; j<3; ++j)
+      std::cout << " "<< B[i+3*j] << " ";
+    std::cout<<std::endl;
+  }
+
+  tensor3_t<double, upper_t, lower_t> C=contract<1,0>(A,A);
+  tensor3_t<double, upper_t, lower_t> D= contract<1,0>(A,B);
+
+  std::cout << "C = " << std::endl; 
+  for(int i=0; i<3; ++i){
+    for(int j=0; j<3; ++j)
+      std::cout << " "<< C[i+3*j] << " ";
+    std::cout<<std::endl;
+  }
+
+  std::cout << "D = " << std::endl; 
+  for(int i=0; i<3; ++i){
+    for(int j=0; j<3; ++j)
+      std::cout << " "<< D[i+3*j] << " ";
+    std::cout<<std::endl;
+  }
+
+
+
+
+  
 
 }
 
